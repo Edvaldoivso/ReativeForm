f76fa1651ea366f91121a7a860efef26e8ca0d96
Edvaldo I Souza <edvaldoivosouza@ymail.com>
2022-05-12 15:51:00 -0300

    ReativoForm master

---
+++ src/app/Pages/cadastro-pessoal/cadastro-pessoal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-pessoal',
+  templateUrl: './cadastro-pessoal.component.html',
+  styleUrls: ['./cadastro-pessoal.component.scss']
+})
+export class CadastroPessoalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HOMEComponent } from './Pages/home/home.component';
+import { CadastroPessoalComponent } from './Pages/cadastro-pessoal/cadastro-pessoal.component';
+import { CadastroPagamentoComponent } from './Pages/cadastro-pagamento/cadastro-pagamento.component';
+import { SucessoComponent } from './Pages/sucesso/sucesso.component';
+import { ErroComponent } from './Pages/erro/erro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HOMEComponent,
+    CadastroPessoalComponent,
+    CadastroPagamentoComponent,
+    SucessoComponent,
+    ErroComponent
   ],
   imports: [
     BrowserModule,
