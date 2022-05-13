7dee592d5f76cbff185de7c4b7041d45adb06b20
Edvaldo I Souza <edvaldoivosouza@ymail.com>
2022-05-13 18:48:04 -0300

    FormularioPessoal

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HOMEComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  Destinos: Array<{
+    Imagem: string;
+    Titulo: string;
+    Subtitulo: string;
+    TextoBtn: string;
+    Rota: string;
+  }> = [
+    {
+      Imagem: './assets/ImagemFundo/Tokio.jpg',
+      Titulo: 'TOKIO',
+      Subtitulo: 'A SN28 te leva em 30 minutos e só',
+      TextoBtn: 'VAMOS PARA TOKIO',
+      Rota: 'CadPessoa',
+    },
+    {
+      Imagem: './assets/ImagemFundo/SaoPaulo.jpg',
+      Titulo: 'SÃO PAULO',
+      Subtitulo: 'A Cidade dos negocios com a SpaceX',
+      TextoBtn: 'JÁ PARA O BRASIL !',
+      Rota: 'CadPessoa',
+    },
+    {
+      Imagem: './assets/ImagemFundo/CapeTown.jpg',
+      Titulo: 'CapeTown',
+      Subtitulo: 'A DOURADA com a nave do Futuro',
+      TextoBtn: 'Voar Para África',
+      Rota: 'CadPessoa',
+    },
+  ];
+
+  //Redirecionamento
+  public Redirect(i: string) {
+    this.router.navigate([i]);
   }
-
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroPagamentoComponent } from './Pages/cadastro-pagamento/cadastro-pagamento.component';
+import { CadastroPessoalComponent } from './Pages/cadastro-pessoal/cadastro-pessoal.component';
+import { ErroComponent } from './Pages/erro/erro.component';
+import { HOMEComponent } from './Pages/home/home.component';
+import { SucessoComponent } from './Pages/sucesso/sucesso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: HOMEComponent },
+  {
+    path: 'CadPagamento',
+    loadChildren: () =>
+      import('./Pages/cadastro-pagamento/cadastro-pagamento.component').then(
+        (c) => c.CadastroPagamentoComponent
+      ),
+  },
+  {
+    path: 'CadPessoa',
+    component: CadastroPessoalComponent,
+  },
+  {
+    path: 'Sucesso',
+    loadChildren: () =>
+      import('./Pages/sucesso/sucesso.component').then(
+        (s) => s.SucessoComponent
+      ),
+  },
+  { path: '404', redirectTo: '*', pathMatch: 'full' },
+  {
+    path: '*',
+    loadChildren: () =>
+      import('./Pages/erro/erro.component').then((e) => e.ErroComponent),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
